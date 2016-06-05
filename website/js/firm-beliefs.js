cea4687a9e8ed48232ff84f92336f9f057b2fe52
Segun Konibire <gunzalez@hotmail.com>
2016-06-05 23:16:32 +0100

    Top level homepage carousel, 80%

--- website/js/firm-beliefs.js
+++ website/js/firm-beliefs.js
@@ -29,12 +29,6 @@
             this.$mainNavContainer.removeClass('animated').height($('.page').height());
         },
 
-
-
-
-
-
-
         resize: function () {
             this.mainNavResize();
         },
@@ -56,10 +50,32 @@
         }
     };
 
+    firmBeliefs.homePage = {
+        $topLevelContainers: $('.top-level-container'),
+        $topLevelNavHtml: $('.top-level-nav'),
+        $triggers: $('.trigger', this.$topLevelNavHtml),
+        init: function(){
+            var self = this;
+            this.$triggers.on('click', function(){
+
+                // highlight this row
+                var $listEl = $(this).parents('li');
+                $('li', self.$topLevelNavHtml).removeClass('active');
+                $listEl.addClass('active');
+                self.$topLevelContainers.removeClass('active');
+
+                // change corresponding image
+                var index = $('li', self.$topLevelNavHtml).index($listEl);
+                self.$topLevelContainers.eq(index).addClass('active');
+            });
+        }
+    };
+
     firmBeliefs.init = function () {
 
         // all init
         firmBeliefs.environment.init();
+        firmBeliefs.homePage.init();
 
         // resize triggers
         $(window).on('resize', function () {
